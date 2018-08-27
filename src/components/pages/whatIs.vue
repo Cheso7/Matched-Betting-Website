469fb7de724665b59c08f97f347082657db095da
Jarryd <jarryd.cheso@gmail.com>
2018-08-27 23:06:16 +0800

    Testing commit through VS Code

--- src/components/pages/whatIs.vue
+++ src/components/pages/whatIs.vue
@@ -43,11 +43,8 @@
 
       </div>
       
-    <article class="message is-dark">
-        <div class="message-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-        </div>
-    </article>
+      <blockquote>Ut venenatis, nisl scelerisque sollicitudin fermentum, quam libero hendrerit ipsum, ut blandit est tellus sit amet turpis.</blockquote>
+
 
     </div>
     </div>
